13ad8d029b542037c44290b7ae356dec7f091039
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-02-26 19:09:43 -0600

    Set home and away scores to start at zero

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [home, setHome] = useState(32);
-  const [away, setAway] = useState(32);
+  const [home, setHome] = useState(0);
+  const [away, setAway] = useState(0);
+  
   return (
     <div className="container">
       <section className="scoreboard">
